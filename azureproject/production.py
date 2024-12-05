558c910bab6db78b8dadc33d6ba7acf46718cd1e
Devendra Rana <32830074+dranaUB@users.noreply.github.com>
2024-12-05 04:50:27 -0500

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -2,9 +2,10 @@ import os
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-# Uncomment the following lines for App Service
-conn_str = os.environ['8eb3800755292f684dcca11e28607dc421eab95189b4e66a0c6c8d9490409162']
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
+conn_str_params['password'] = '8eb3800755292f684dcca11e28607dc421eab95189b4e66a0c6c8d9490409162'
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=conn_str_params['user'],
     dbpass=conn_str_params['password'],
