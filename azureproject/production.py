2cb00c61c3ce3d2d8c29e8f264c4163a1c267448
Devendra Rana <32830074+dranaUB@users.noreply.github.com>
2024-12-05 05:13:22 -0500

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -5,7 +5,6 @@ import os
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-conn_str_params['password'] = '8eb3800755292f684dcca11e28607dc421eab95189b4e66a0c6c8d9490409162'
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=conn_str_params['user'],
     dbpass=conn_str_params['password'],
