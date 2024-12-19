436c690c56956d0ad8063dd76efbef251fced647
Devendra Rana <32830074+dranaUB@users.noreply.github.com>
2024-12-19 09:22:31 -0500

    Update production.py
    back to original

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,19 +1,10 @@
 import os
-# conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# # Configure Postgres database based on connection string of the libpq Keyword/Value form
-# # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-# conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-
-# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=conn_str_params['user'],
-#     dbpass=conn_str_params['password'],
-#     dbhost=conn_str_params['host'],
-#     dbname=conn_str_params['dbname']
-# )
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
+    dbuser=conn_str_params['user'],
+    dbpass=conn_str_params['password'],
+    dbhost=conn_str_params['host'],
+    dbname=conn_str_params['dbname']
 )
